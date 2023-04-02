1fb561dc1bbba264b48e09eecc705f93ef22e97e
lm <mueller.lutz@gmail.com>
2023-04-02 03:25:50 +0200

    update jdk, kotlin, spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.1"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.4.21"
-    kotlin("plugin.spring") version "1.4.21"
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "io.github.lumue"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
     compileOnly {
@@ -30,12 +30,13 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jsoup:jsoup:1.11.3")
+    implementation("org.jsoup:jsoup:1.15.4")
     implementation("org.slf4j:slf4j-api:1.7.10")
-    implementation("org.apache.httpcomponents:httpclient:4.5.5")
-    implementation("ru.gildor.coroutines:kotlin-coroutines-retrofit:0.13.0-eap13")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.6")
-    implementation("org.springdoc:springdoc-openapi-ui:1.5.2")
+    implementation("org.apache.httpcomponents:httpclient:4.5.14")
+    implementation("ru.gildor.coroutines:kotlin-coroutines-retrofit:1.1.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.2")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.15")
+
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(kotlin("test"))
@@ -44,24 +45,24 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+   useJUnitPlatform()
 }
 
 tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
     docker {
 
-        imageName = "lumue/metadata-resolver"
-        isPublish=true
+        imageName.set("lumue/metadata-resolver")
+        publish.set(true)
         publishRegistry {
-            username = "lumue"
-            password = "ddlom85s"
-            url = "https://registry.docker-hub.com/v2/"
-            email = "mueller.lutz@gmail.com"
+            username.set("lumue")
+            password.set("M9w8a+ET9u@+tA%")
+            url.set("https://registry.docker-hub.com/v2/")
+            email.set("mueller.lutz@gmail.com")
         }
     }
 }
